90370a8dfb3b848e54446dc9629ae0f04dce7435
Sgeo <sgeoster@gmail.com>
2023-09-11 19:27:52 -0400

    Able to produce some audio.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 static CHAIN_CHARS_STR: &'static str = "1234567890#@/STUVWXYZ‡,%JKLMNOPQR-$*ABCDEFGHI+.⌑";
 static CHAIN_CHARS: once_cell::sync::Lazy<Vec<char>> = once_cell::sync::Lazy::new(|| {
     CHAIN_CHARS_STR.chars().collect()
@@ -62,8 +64,13 @@ impl Printer {
     }
 }
 
+fn sample_from_time(time: u64) -> u64 {
+    44100 * time / 1000000
+}
+
 fn main() -> anyhow::Result<()> {
     let mut printer = Printer::new();
+    let mut times_microseconds: VecDeque<u64> = VecDeque::new();
     for line in std::io::stdin().lines() {
         let line = line?;
         let mut prior_time: u64 = 0;
@@ -73,10 +80,33 @@ fn main() -> anyhow::Result<()> {
                 println!("Printing at time {}, delta = {}", printer.time, printer.time - prior_time);
                 println!("{}", printed);
                 prior_time = printer.time;
+                times_microseconds.push_back(printer.time);
             }
             printer.advance();
         }
     }
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    let mut writer = hound::WavWriter::create("out.wav", spec)?;
+
+    eprintln!("Number of samples: {}", sample_from_time(*times_microseconds.back().unwrap()));
+
+    for sample in 0..=sample_from_time(*times_microseconds.back().unwrap()) {
+        if sample == sample_from_time(*times_microseconds.front().unwrap()) {
+            times_microseconds.pop_front();
+            writer.write_sample(i16::MAX)?;
+        } else {
+            writer.write_sample(0)?;
+        }
+    }
+
+    writer.finalize()?;
 
     Ok(())
 }
